a5eeb3c69e7a82f491d821c75b4c91de537bbf81
Suntae Kim <kst6294@gmail.com>
2021-08-26 17:56:22 +0900

    사용자 모델 수정

--- version_02/api/user/models.py
+++ version_02/api/user/models.py
@@ -1,4 +1,28 @@
 from django.db    import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User Must Have an Email Address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 
 class Language(models.Model):
@@ -13,15 +37,30 @@ class Country(models.Model):
         db_table = 'countries'
 
 
-class User(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
+class User(AbstractBaseUser, PermissionsMixin):
+    nickname = models.CharField(max_length=20)
+    email = models.EmailField(
+        verbose_name="Customer Email", 
+        help_text="Required and Unique", 
+        unique=True
+    )
+    
+    password = models.CharField(
+        verbose_name="Customer Password",
+        help_text="Required and Secured",
+        max_length=255
+    )
+
     profile_image_url = models.URLField(null=True)
     language = models.ForeignKey("Language", on_delete=models.PROTECT)
     country = models.ForeignKey("Country", on_delete=models.PROTECT)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        db_table = 'users'
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
